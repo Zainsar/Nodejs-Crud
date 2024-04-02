e89aed5613777b9a7871f3189d7bb108e470259d
Zain Sarfraz <zainsarfraz82@gmail.com>
2024-04-02 22:53:04 +0500

    Nodejs/Crud

---
+++ index.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const productRoutes = require("./routes/product");
+
+
+dotenv.config();
+
+
+// connect to db
+// mongoose.connect(
+//   process.env.DB_CONNECT,
+//   { useNewUrlParser: true, useUnifiedTopology: true },
+//   () => console.log("connected to db")
+// );
+
+// mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.DB_CONNECT, {
+      useNewUrlParser: true,
+    });
+    console.log(`MongoDB Connected: {conn.connection.host}`);
+  } catch (error) {
+    console.error(error.message);
+    // process.exit(1);
+  }
+}
+
+connectDB();
+
+// Import routes
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+
+// route Middlewares
+app.use("/api/products", productRoutes);
+
+app.listen(8080, () => console.log("server up and runing on port 4000!"));
